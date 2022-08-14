93a6fb4750f63b3cd29138dd134cdaf89461d05a
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-08-14 20:05:01 +0400

    Add files via upload

---
+++ Custom/script.js
@@ -0,0 +1,48 @@
+
+const arr = [5, 7, 9, 1, 4];
+
+
+const popFunc = function (arr, index) {
+    const NewArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i === index) {
+            i++;
+        }
+        NewArr.push(arr[i]);
+    }
+    if(index>arr.length-1){
+        console.log("Uygun element yoxdur");
+    }
+    if(index===arr.length-1){
+        NewArr.pop();
+    }
+    return NewArr;
+}
+const result = popFunc(arr, 5);
+
+console.log(result);
+
+
+
+
+const myArr1 = [6, 1, 2, 6, 4];
+
+
+const pushFunc = function (arr, index, value) {
+    const NewArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === index) {
+            NewArr.push(value)
+        }
+        NewArr.push(arr[i]);
+    }
+    if (index > arr.length-1) {
+
+        NewArr.push(value)
+    }
+    return NewArr;
+}
+const result1 = pushFunc(myArr1, 7, `Salam`);
+
+console.log(result1);
